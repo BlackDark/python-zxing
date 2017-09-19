3581774482e0e4a4613dacc309b3f9397ff63d81
Eduard Marbach <eduard.marbach@daimler.com>
2017-09-19 16:21:22 +0200

    Fix windows bug with URI hierarchy exception

--- zxing/__init__.py
+++ zxing/__init__.py
@@ -7,10 +7,13 @@
 #
 
 from __future__ import print_function
+
+import pathlib
 from enum import Enum
 
 __version__ = '0.4'
 import subprocess as sp, re, os
+
 
 class BarCodeReader(object):
   cls = "com.google.zxing.client.j2se.CommandLineRunner"
@@ -20,26 +23,27 @@ class BarCodeReader(object):
     if classpath:
       self.classpath = classpath if isinstance(classpath, str) else ':'.join(classpath)
     elif "ZXING_CLASSPATH" in os.environ:
-      self.classpath = os.environ.get("ZXING_CLASSPATH","")
+      self.classpath = os.environ.get("ZXING_CLASSPATH", "")
     else:
       self.classpath = os.path.join(os.path.dirname(__file__), 'java', '*')
 
-  def decode(self, filenames, try_harder = False, possible_formats = None):
+  def decode(self, filenames, try_harder=False, possible_formats=None):
     possible_formats = (possible_formats,) if isinstance(possible_formats, str) else possible_formats
 
     if isinstance(filenames, str):
       one_file = True
-      filenames = [ os.path.abspath(filenames) ]
+      filenames = [pathlib.Path(os.path.abspath(filenames)).as_uri()]
     else:
       one_file = False
-      filenames = [ os.path.abspath(f) for f in filenames ]
+      filenames = [pathlib.Path(os.path.abspath(f)).as_uri() for f in filenames]
 
     cmd = [self.java, '-cp', self.classpath, self.cls] + filenames
+
     if try_harder:
       cmd.append('--try_harder')
     if possible_formats:
       for pf in possible_formats:
-        cmd += ['--possible_formats', pf ]
+        cmd += ['--possible_formats', pf]
 
     p = sp.Popen(cmd, stdout=sp.PIPE, universal_newlines=True)
     stdout, stderr = p.communicate()
@@ -48,15 +52,17 @@ class BarCodeReader(object):
       raise sp.CalledProcessError(p.returncode, p.args, stdout, stderr)
     else:
       file_results = re.split(r'\nfile:', stdout)
-      codes = [ BarCode.parse(result) for result in file_results ]
+      codes = [BarCode.parse(result) for result in file_results]
 
     return (codes[0] if one_file else codes)
+
 
 class CLROutputBlock(Enum):
   UNKNOWN = 0
   RAW = 1
   PARSED = 2
   POINTS = 3
+
 
 class BarCode(object):
   @classmethod
@@ -67,7 +73,7 @@ class BarCode(object):
     points = []
 
     for l in zxing_output.splitlines(True):
-      if block==CLROutputBlock.UNKNOWN:
+      if block == CLROutputBlock.UNKNOWN:
         if l.endswith(': No barcode found\n'):
           return None
         m = re.search(r"format:\s*([^,]+),\s*type:\s*([^)]+)", l)
@@ -75,17 +81,17 @@ class BarCode(object):
           format, type = m.groups()
         elif l.startswith("Raw result:"):
           block = CLROutputBlock.RAW
-      elif block==CLROutputBlock.RAW:
+      elif block == CLROutputBlock.RAW:
         if l.startswith("Parsed result:"):
           block = CLROutputBlock.PARSED
         else:
           raw += l
-      elif block==CLROutputBlock.PARSED:
+      elif block == CLROutputBlock.PARSED:
         if re.match(r"Found\s+\d+\s+result\s+points?", l):
           block = CLROutputBlock.POINTS
         else:
           parsed += l
-      elif block==CLROutputBlock.POINTS:
+      elif block == CLROutputBlock.POINTS:
         m = re.match(r"\s*Point\s*\d+:\s*\(([\d.]+),([\d.]+)\)", l)
         if m:
           points.append((float(m.group(1)), float(m.group(2))))
@@ -100,4 +106,5 @@ class BarCode(object):
     self.points = points
 
   def __repr__(self):
-    return '{}({!r}, {!r}, {!r}, {!r}, {!r})'.format(self.__class__.__name__, self.raw, self.parsed, self.format, self.type, self.points)
+    return '{}({!r}, {!r}, {!r}, {!r}, {!r})'.format(self.__class__.__name__, self.raw, self.parsed, self.format,
+                                                     self.type, self.points)
